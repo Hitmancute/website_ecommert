96151f2c1423c91bfb2e8265a738fdb3b1b3154c
Hitmancute <fuadmatang1@gmail.com>
2026-03-30 13:14:21 +0700

    fist commit

---
+++ app/View/Components/Sidebar.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Sidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $links;
+    public function __construct()
+    {
+        $this->links = [
+            [
+                'type'  => 'section',
+                'title'  => 'Home',
+                'icon'  => 'ti-dots',
+            ],
+            [
+                'type'  => 'menu',
+                'name'  => 'Dashboard',
+                'route'  => 'home',
+                'active'  => 'home',
+                'icon'  => 'ti-brand-windows',
+            ],
+        ];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.sidebar');
+    }
+}
